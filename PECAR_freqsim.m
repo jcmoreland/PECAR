7fc38f2c4ab13b4eea1ecabc35cdc43948191a65
james.moreland <jamesm37@uw.edu>
2017-10-18 23:00:54 -0700

    Attention frequency sim
    Still not really based on real data but runs n simulations of the observers.

--- PECAR_freqsim.m
+++ PECAR_freqsim.m
@@ -4,7 +4,12 @@
 % attempt to reconstruct it.
 clear all
 
-%% The frequency analysis occurs on the pdif so start by creating a pdif
+nsim = 1000; % How many times to run the subject groups through the analysis
+
+nsubj = 15;
+noise = 1; % 0 is no noise
+
+% The frequency analysis occurs on the pdif so start by creating a pdif
 % vector.
 dt_sampled = .04;
 maxt_sampled = .520;
@@ -28,7 +33,7 @@ freqs = f(2:end); % sampled frequencies
 
 nCycles = 5; % Make this the period of the effect
 amp = 1;
-phase = 30;
+phase = 0;
 
 pdif = amp*sin(2*pi*t*nCycles-pi*phase/180);
 
@@ -47,28 +52,36 @@ set(gca,'XTick',0:.1:maxt);
 set(gca,'YTick',-1.5:1.5);
 ylabel('P1/P2 Difference');
 set(gca,'YLim',amp*3*[-1,1]);
-%% Now create several runs
-nsubj = 15;
-sd = 1; % 0 is no noise
 
-for ss = 1:nsubj
-    % added noise
-    pdif_group(ss,:) = pdif(t_loc) + sd*randn(1,nt_sampled);
+%% Now simulate groups of observers
+for i = 1:nsim
     
-    %% Run the same analysis as on the real data on this noisy pdif
-    F = fft(pdif_group(ss,:)); % Only use the time points sampled in the experiment
+    for ss = 1:nsubj
+        % added noise
+        pdif_group(ss,:) = pdif(t_loc) + noise*randn(1,nt_sampled);
+        
+        %% Run the same analysis as on the real data on this noisy pdif
+        F = fft(pdif_group(ss,:)); % Only use the time points sampled in the experiment
+        
+        A = complex2real(F,t(t_loc));
+        
+        pdif_amps(ss,:) = A.amp(1:end-1);
+        
+    end
+    pdiff_mean_tmp(i,:) = mean(pdif_group);
+    pdiff_se_tmp(i,:) = std(pdif_group,1)/sqrt(nsubj);
     
-    A = complex2real(F,t(t_loc));
+    pdif_amps_mean_tmp(i,:) = mean(pdif_amps);
+    pdif_amps_se_tmp(i,:) = std(pdif_amps)/sqrt(nsubj);
     
-    pdif_amps(ss,:) = A.amp(1:end-1);
-
+    maxampfreq(i) = freqs(find(pdif_amps_mean_tmp(i,:) == max(pdif_amps_mean_tmp(i,:)))); % 
 end
 
-pdiff_mean = mean(pdif_group);
-pdiff_se = std(pdif_group,1)/sqrt(nsubj);
+pdiff_mean = mean(pdiff_mean_tmp);
+pdiff_se = mean(pdif_group);
 
-pdif_amps_mean = mean(pdif_amps);
-pdif_amps_se = std(pdif_amps)/sqrt(nsubj);
+pdif_amps_mean = mean(pdif_amps_mean_tmp);
+pdif_amps_se = mean(pdif_amps_se_tmp);
 
 % Plot the mean data of pdif with periodicity
 subplot(3,1,2)
@@ -90,3 +103,11 @@ ylabel('Amplitude')
 title('Real data: Frequency power')
 % Add a line at the real frequency of this fake data
 plot([nCycles,nCycles],[0,max(pdif_amps(:))],'k:','LineWidth',2)
+
+figure(3)
+clf
+hold on
+hist(maxampfreq)
+set(gca,'XTick',freqs,'XTickLabel',round(freqs,3,'Significant'),...
+    'XLim',[0,max(freqs)+1],'YLim',[0,nsim+20])
+
